f5e4abe7d48afe3beb698fd933a8afd679dc7669
Luis Pabón <luis@portworx.com>
2019-11-20 19:06:20 -0500

    set and most of get

--- pkg/skvdb/skvdb.go
+++ pkg/skvdb/skvdb.go
@@ -1,5 +1,6 @@
 /*
 Copyright 2019 Isabella Pabón <isabella@chrysalix.org>
+Copyright 2019 Luis Pabón <lpabon@chrysalix.org>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,6 +19,10 @@ package skvdb
 import (
 	"context"
 	"fmt"
+	"sync"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Skvdb interface {
@@ -25,23 +30,72 @@ type Skvdb interface {
 	Get(ctx context.Context, key []byte) ([]byte, error)
 }
 
+type Element struct {
+	Value []byte
+	Owner string
+}
+
 type skdvb struct {
-	db map[string]string
+	lock sync.Mutex
+	db map[string]*Value
 }
 
 func New() Skvdb {
 	return &skdvb{
-		db: make(map[string]string),
+		db: make(map[string]*Value),
+	}
+}
+
+func (s *skdvb) Set(ctx context.Context, key, value string) error  {
+
+	// Get the key first to check if it exists and to check for permission
+	_, found, err := get(ctx, key) 
+	if err != nil {
+		return err
 	}
 
+	// Get user information from context
+	username, ok := getUser(ctx)
+	if !ok {
+		return status.Errorf(codes.Internal, "Unable to determine user information")
+	}
+
+	// Create an element
+	e := &Element{
+		Value: value,
+		Owner: username,
+	}
+
+	// Save element to database
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.db[key] = e
+
+	return nil
 }
 
-func (s *skdvb) Set(ctx context.Context, key []byte, value []byte) error  {
-	return fmt.Errorf("not done yet")
+func (s *skdvb) Get(ctx context.Context, key string) (string, error)  {
+	// Save element to database
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+		// Check permission
+		username, ok := getUser(ctx)
+		if !ok {
+		return "", status.Errorf(codes.Internal, "Unable to determine user information")
+		}
+
+	if e, ok := s.db[key]; !ok {
+		return "", status.Errorf(codes.NotFound, "Key %s was not found", key)
+	} else {
+		if e.Owner != username {
+			return "", status.Errorf(codes.PermissionDenied, "Access denied to key %s", key)
+		}
+	}
 	
+	return nil, nil
 }
 
-func (s *skdvb) Get(ctx context.Context, key []byte) ([]byte, error)  {
-	return nil, nil 
-	
+func getUser(ctx context.Context) (string, bool) {
+	return ctx.Value("username").(string)
 }
